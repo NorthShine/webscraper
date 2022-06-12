94ccc2e2296fedf548f54a2f1e3cc29c6e3f9681
Paul <52328887+paulbraam@users.noreply.github.com>
2022-06-12 14:47:38 +0300

    Update route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,6 @@ import express from 'express';
 import { getContent } from '../controllers';
 const router = express.Router();
 
-router.get('/api/v1/', getContent);
+router.get('parser/api/v1/', getContent);
 
 export default router;
